388d527563747731f17199c05e74ae0381154ea8
Karina Agliullova <karinaar4299@gmail.com>
2022-03-11 00:17:50 -0500

    Modified js and html files

--- app/static/js/manageDepartments.js
+++ app/static/js/manageDepartments.js
@@ -1,7 +1,7 @@
 // Opens collapse menu for this page
 $("#admin").collapse("show");
 
-$(document).ready( function(){
+$(document).ready( function() {
     x = $('#departmentsTable');
     //console.log(x);
     x.DataTable({
@@ -11,27 +11,27 @@ $(document).ready( function(){
 
 function status(department, dept_name) {
 /*
- POSTs the compliance status change for the department. Updates UI with correct button and feedback to user.
+ POSTs the compliance status change for the department.
+ Updates UI with correct button and feedback to user.
  PARAMS:
     int department: department ID
     str dept_name: Name of the department
 
  RETURNS: None
 */
-  $.ajax({
-    method: "POST",
-    url: "/admin/complianceStatus",
-    dataType: "json",
-    contentType: "application/json",
-    data: JSON.stringify({"deptName": department}),
-    processData: false,
-    success: function(response) {
-//      console.log(response);
-      if(response["Success"]) {
+  $.ajax({method: "POST",
+          url: "/admin/complianceStatus",
+          dataType: "json",
+          contentType: "application/json",
+          data: JSON.stringify({"deptName": department}),
+          processData: false,
+          success: function(response) {
+      // console.log(response);
+      if (response["Success"]) {
         category = "info"
-        var departmentID = $("#dept_btn_" + department);
-        //console.log(departmentID);
-        if ($(departmentID).hasClass("btn-success")){
+        let departmentID = $("#dept_btn_" + department);
+        // console.log(departmentID);
+        if ($(departmentID).hasClass("btn-success")) {
           $(departmentID).removeClass("btn-success");
           $(departmentID).addClass("btn-danger");
           $(departmentID).text("Not in Compliance");
@@ -50,7 +50,8 @@ function status(department, dept_name) {
 
         $("#flash_container").html('<div class="alert alert-'+ category +'" role="alert" id="flasher">'+msg+'</div>');
         $("#flasher").delay(3000).fadeOut();
-//        $('#departmentsTable').DataTable().ajax.reload();     #FIXME the table doesn't sort correctly after the ajax response.
+        // $('#departmentsTable').DataTable().ajax.reload();
+        // #FIXME the table doesn't sort correctly after the ajax response.
       }
     }
   })
