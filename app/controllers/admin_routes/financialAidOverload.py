93fb5f9eb679e8c3f488dd7381a67bf002195ffa
Sreynit Khatt <khatts@berea.edu>
2022-10-12 15:45:52 -0400

    display notes from FA and SAAS. Label notes type according to type of user

--- app/controllers/admin_routes/financialAidOverload.py
+++ app/controllers/admin_routes/financialAidOverload.py
@@ -36,7 +36,7 @@ def financialAidOverload(formHistoryID):
                                 .join(FormHistory)
                                 .where(FormHistory.status_id.in_(["Approved", "Approved Reluctantly", "Pending"]),
                                        FormHistory.historyType_id == "Labor Status Form",
-                                       LaborStatusForm.studentSupervisee == lsfForm.studentSupervisee.ID, 
+                                       LaborStatusForm.studentSupervisee == lsfForm.studentSupervisee.ID,
                                        LaborStatusForm.termCode == lsfForm.termCode))
     totalHours = {"secondaryHours" : 0, "primaryHours": 0}
     supervisor = department = ""
@@ -54,8 +54,9 @@ def financialAidOverload(formHistoryID):
     userDept = "Financial Aid"
     if currentUser.isSaasAdmin:
         userDept = "SAAS"
-
 # will need to add term to the interface and then have a prefill variable
+
+    notesList = Notes.select().where(Notes.formID == lsfForm)
     return render_template( 'admin/financialAidOverload.html',
                         overloadFormHistory = overloadFormHistory,
                         lsfForm = lsfForm,
@@ -64,7 +65,8 @@ def financialAidOverload(formHistoryID):
                         department = department,
                         supervisor= supervisor,
                         contractDate = contractDate,
-                        totalOverloadHours = totalHours["primaryHours"] + totalHours["secondaryHours"]
+                        totalOverloadHours = totalHours["primaryHours"] + totalHours["secondaryHours"],
+                        notesList = notesList
                       )
 
 @admin.route("/admin/financialAidOverloadApproval/<status>", methods=["POST"])
