09b56f2496955f9d79000078e8a36a2e7ee366c8
Tyler Parton <tylerpar99@gmail.com>
2022-10-17 08:29:51 -0400

    Working on finding origin of problem.

--- app/controllers/main_routes/studentLaborEvaluation.py
+++ app/controllers/main_routes/studentLaborEvaluation.py
@@ -47,6 +47,7 @@ class SLEForm(FlaskForm):
 
 @main_bp.route('/sle/<statusKey>', methods=['GET', 'POST'])
 def sle(statusKey):
+    print("Here")
     # NOTE: statusKey is the LSF id. Everything after this (template and controller) uses the associated laborHistory object/ID for this LSF id.
     currentUser = require_login()
 
@@ -54,7 +55,8 @@ def sle(statusKey):
     if currentUser.student and currentUser.student.ID != laborHistoryForm.formID.studentSupervisee.ID:
         # current user is not the student
         return render_template('errors/403.html'), 403
-    elif currentUser.student == None and currentUser.supervisor.DEPT_NAME != laborHistoryForm.formID.supervisor.DEPT_NAME:
+    elif currentUser.student == None and laborHistoryForm.formID.supervisor.DEPT_NAME not in list(getDepartmentsForSupervisor(currentUser)):
+        print(list(getDepartmentsForSupervisor(currentUser)))
         # current user is not in the same dept as the lsf supervisor
         return render_template('errors/403.html'), 403
 
--- app/logic/buttonStatus.py
+++ app/logic/buttonStatus.py
@@ -46,7 +46,7 @@ class ButtonStatus:
                 elif not evaluation.is_midyear_evaluation:  #i.e., it's a final evaluation
                     self.evaluation_exists = True
         else:
-            if ogHistoryForm.formID.supervisor.DEPT_NAME == currentUser.supervisor.DEPT_NAME:
+            if ogHistoryForm.formID.supervisor.DEPT_NAME in list(getDepartmentsForSupervisor(currentUser)):
                 if historyForm.formID.termCode.isFinalEvaluationOpen or historyForm.formID.termCode.isMidyearEvaluationOpen:
                     self.evaluate = True
                 for evaluation in evaluations:
