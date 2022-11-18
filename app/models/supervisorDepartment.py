095a6ade838ba8a9da8dbdcb6516385b602ffc6c
bledsoef <bledsoef@berea.edu>
2022-11-18 15:56:35 -0500

    Fiddled with models

--- app/models/supervisorDepartment.py
+++ app/models/supervisorDepartment.py
@@ -1,8 +1,7 @@
 from app.models import *
 from app.models.supervisor import Supervisor
 from app.models.department import Department
-from peewee import CharField
 
 class SupervisorDepartment(baseModel):
-        supervisor          = ForeignKeyField(Supervisor)
-        department          = ForeignKeyField(Department)
+    supervisor = ForeignKeyField(Supervisor, null=True)
+    department = ForeignKeyField(Department)
