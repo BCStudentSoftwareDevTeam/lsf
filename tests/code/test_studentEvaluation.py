dff6a15f421a403003cfa5c35ba6c632f2720d61
Tyler Parton <tylerpar99@gmail.com>
2023-02-03 16:27:22 -0500

    Working on testing suite.

--- tests/code/test_studentEvaluation.py
+++ tests/code/test_studentEvaluation.py
@@ -5,13 +5,44 @@ import pytest
 from app.models import mainDB
 from app.models.laborStatusForm import LaborStatusForm
 from app.models.formHistory import FormHistory
+from app.models.student import Student
+from app.models.supervisor import Supervisor
+from app.logic.userInsertFunctions import createLaborStatusForm
+
 import json
 
 
 @pytest.mark.integration
 def testCreateStudentEval():
     with mainDB.atomic() as transaction:
+        supervisorDept = Department.create(DEPT_NAME="Testing Department", ACCOUNT="6740", ORG="2114")
+        testingTerm = Term.create(termCode = 200000, termName = "Testing Term")
+        testingStudent =  (Student.create(
+                ID="B00000002",
+                FIRST_NAME="Tyler",
+                LAST_NAME="Parton",
+                STU_EMAIL="partont@whatever.edu",
+                STU_CPO="Unknown"
+                ))
+        testingSupervisor = (Supervisor.create(ID = "B00000001",
+                            PIDM = 75,
+                            FIRST_NAME = "Not",
+                            LAST_NAME = "Scott",
+                            EMAIL = "None",
+                            CPO = "None",
+                            DEPT_NAME = "Testing Department"))
+        testingFormHistory = (FormHistory.create(formID = inDeptForm.laborStatusFormID,
+                                               historyType = "Labor Status Form",
+                                               releaseForm = None,
+                                               adjustedForm = None,
+                                               overloadForm = None,
+                                               createdBy = 1,
+                                               createdDate = "2020-04-14",
+                                               reviewedDate = None,
+                                               reviewedBy = None,
+                                               status = "Approved"))
 
+        createLaborStatusForm("B00000002", "B00000001", supervisorDept.ID, testingTerm)
         with app.test_request_context(
             "/sle", method="POST", data=termCodeDict):
                 createSLEform = sle()
