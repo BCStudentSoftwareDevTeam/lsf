5266a56f1849c0d3345a0e30124ba42342ae4d68
Brian Ramsay <ramsayb2@berea.edu>
2023-08-29 10:01:31 -0400

    alternate quotes to avoid escaping

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,6 +1,6 @@
 function msgFlash(flash_message, status){
     var category = (status === "success") ? "success" : ((status === "fail") ? "danger" : "warning");
-    $("#flash_container").prepend("<div class=\"alert alert-"+ category +"\" role=\"alert\" id=\"flasher\">"+flash_message+"</div>");
+    $("#flash_container").prepend('<div class="alert alert-'+ category +'" role="alert" id="flasher">'+flash_message+'</div>');
     $("#flasher").delay(5000).fadeOut();
 }
 
