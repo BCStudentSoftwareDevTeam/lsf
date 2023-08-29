468ab4e3da8a8370c751adf2f354bd713088ad99
Brian Ramsay <ramsayb2@berea.edu>
2023-08-29 09:50:33 -0400

    shrink msgFlash

--- app/static/js/base.js
+++ app/static/js/base.js
@@ -1,19 +1,6 @@
 function msgFlash(flash_message, status){
-    if (status === "success") {
-        category = "success";
-        $("#flash_container").prepend("<div class=\"alert alert-"+ category +"\" role=\"alert\" id=\"flasher\">"+flash_message+"</div>");
-        $("#flasher").delay(5000).fadeOut();
-    }
-    else if (status === "fail"){
-        category = "danger";
-        $("#flash_container").prepend("<div class=\"alert alert-"+ category +"\" role=\"alert\" id=\"flasher\">"+flash_message+"</div>");
-        $("#flasher").delay(5000).fadeOut();
-    }
-    else if (status === "warning") {
-        category = "warning"
-        $("#flash_container").prepend("<div class=\"alert alert-"+ category +"\" role=\"alert\" id=\"flasher\">"+flash_message+"</div>");
-        $("#flasher").delay(5000).fadeOut();
-    }
-
+    var category = (status === "success") ? "success" : ((status === "fail") ? "danger" : "warning");
+    $("#flash_container").prepend("<div class=\"alert alert-"+ category +"\" role=\"alert\" id=\"flasher\">"+flash_message+"</div>");
+    $("#flasher").delay(5000).fadeOut();
 }
 
