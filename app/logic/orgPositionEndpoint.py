8d0e6e719b94c60c971ea5ee6dbe2563d35bd00e
Andersonstettner <stettnera30@gmail.com>
2023-08-30 10:16:07 -0400

    renamed variable

--- app/logic/orgPositionEndpoint.py
+++ app/logic/orgPositionEndpoint.py
@@ -15,10 +15,10 @@ def getPositionInfo(orgCode):
                               .where(Department.ORG == orgCode))
     laborFormDict = {}
     for deptCode in givenOrgCode:
-        celtsLabor = (LaborStatusForm.select(LaborStatusForm.studentSupervisee_id, LaborStatusForm.termCode_id,  LaborStatusForm.jobType, LaborStatusForm.WLS, LaborStatusForm.POSN_TITLE)
-                                     .where(LaborStatusForm.department_id == deptCode))
+        deptLabor = (LaborStatusForm.select(LaborStatusForm.studentSupervisee_id, LaborStatusForm.termCode_id,  LaborStatusForm.jobType, LaborStatusForm.WLS, LaborStatusForm.POSN_TITLE)
+                                    .where(LaborStatusForm.department_id == deptCode))
         
-        for laborForm in celtsLabor:
+        for laborForm in deptLabor:
             if laborForm.studentSupervisee_id not in laborFormDict:
                 laborFormDict[laborForm.studentSupervisee_id] = []
             laborFormDict[laborForm.studentSupervisee_id].append((laborForm.termCode_id, laborForm.POSN_TITLE, laborForm.jobType, laborForm.WLS))
