c40ff3546370a1c8b12513176f209e4a2bd864ec
Brian Ramsay <ramsayb2@berea.edu>
2023-08-31 09:07:14 -0400

    Update laborhistory.js

--- app/static/js/laborhistory.js
+++ app/static/js/laborhistory.js
@@ -7,13 +7,10 @@ function goback(departmentName){
 }
 
 $('#positionTable tbody tr td').on('click',function(){
-  /*If boolean value is false, flash container letting user know that they do not
-  have access. Else, load student labor history modal.*/
  if (this.getAttribute('value') == 'false') {
    $("#flash_container").html('<div class="alert alert-danger" role="alert" id="flasher">You do not have access to this department\'s information.</div>');
    $("#flasher").delay(3000).fadeOut();
- }
- else {
+ } else {
    loadLaborHistoryModal(this.id)
  }
 });
@@ -33,21 +30,20 @@ function redirection(laborStatusKey){
   $("#rehire").attr("href", "/laborstatusform/" + laborStatusKey); // will go to the lsf controller
   $("#release").attr("href", "/laborReleaseForm/" + laborStatusKey); // will go to labor release form controller
   $("#sle").attr("href", "/sle/" + laborStatusKey); // will go to student labor evaluations controller
-  // console.log(laborStatusKey);
 }
 
 function mailPositionToLabor(){
-  let subject = `Labor Position Information for ${$('#studentDetails').text()}`
+  let subject = `Labor Position Question about ${$('#studentDetails').text()}`
   subject= subject.replace(/\s+/g, ' ')
-  let body = `Student Name:${$('#studentDetails').text()}%0D%0A`+
+  let body = `%0D%0A%0D%0AStudent Name:${$('#studentDetails').text()}%0D%0A`+
              `Term: ${$('#laborTerm').text()}%0D%0A`+
-             `Department (ID): ${$('#laborDepartment').text()}%0D%0A`+
              `Supervisor: ${$('#laborSupervisor').text()}%0D%0A`+
+             `Department (ID): ${$('#laborDepartment').text()}%0D%0A`+
              `Job Type (Hours): ${$('#laborJobType').text()}%0D%0A`+
              `Position (WLS): ${$('#laborPosition').text()}%0D%0A`+
              `Start Date: ${$('#laborStartDate').text()}%0D%0A`+
              `End Date: ${$('#laborEndDate').text()}`
-  body = body.replace(/\s+/g, ' ')
+  body = body.replace(/\s+/g, '%20')
   window.location.href = `mailto:labor@berea.edu?subject=${subject}&body=${body}`;
 
 }
