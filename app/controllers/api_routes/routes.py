4cfc289ee276fa78b1975bea2a1f4da6ad1deaa9
Andersonstettner <stettnera30@gmail.com>
2023-09-15 14:22:00 -0400

    combined the two similar functions into one

--- app/controllers/api_routes/routes.py
+++ app/controllers/api_routes/routes.py
@@ -3,19 +3,21 @@ from app.models.department import Department
 from app.models.student import Student
 
 from app.controllers.api_routes import api_bp
-from app.logic.apiEndpoint import getFormsForOrg, getFormsForStudent
+from app.logic.apiEndpoint import getLaborInformation
 
 @api_bp.route('/api/org/<orgCode>', methods=['GET'])
 def getLaborForms(orgCode):
     # TODO: Need to add authentication
     if Department.select().where(Department.ORG == orgCode).exists():
-        return getFormsForOrg(orgCode)
+        return getLaborInformation(orgCode = orgCode)
     else: 
         abort(404)
 
 @api_bp.route('/api/usr/<bNumber>', methods=['GET'])
 def getUserLaborForm(bNumber):
+    # TODO: Need to add authentication
+
     if Student.select().where(Student.ID == bNumber).exists():
-        return getFormsForStudent(bNumber)
+        return getLaborInformation(bNumber = bNumber)
     else:
         abort(404)
--- app/logic/apiEndpoint.py
+++ app/logic/apiEndpoint.py
@@ -6,15 +6,17 @@ from app.models.department import Department
 from app.models.formHistory import FormHistory
 from app.models.term import Term
 
-def getFormsForOrg(orgCode):
-    '''
-    Returns relevant labor information for CELTS labor students to be sent to CELTS-Link
-    '''
-    # TODO: Write test 
+def getLaborInformation(orgCode = "", bNumber = ""):
+    #TODO: need to add tests. 
+
+    if not orgCode:
+        # The default right now is just to search within CELTS Departments, so setting it 
+        # here if it is not passed in. 
+        orgCode = 2084
 
     givenOrgCode = (Department.select(Department.departmentID)
                               .where(Department.ORG == orgCode))
-
+    
     deptLabor = (LaborStatusForm.select(LaborStatusForm.studentSupervisee_id, 
                                         LaborStatusForm.termCode, 
                                         LaborStatusForm.POSN_TITLE,
@@ -27,42 +29,10 @@ def getFormsForOrg(orgCode):
                                 .join(FormHistory)
                                 .where(LaborStatusForm.department_id.in_(givenOrgCode), 
                                        FormHistory.status_id == "Approved")
-                                .order_by(LaborStatusForm.termCode_id))        
-    
-    laborFormDict = {}
-    for laborForm in deptLabor:
-        if laborForm.studentSupervisee_id not in laborFormDict:
-            laborFormDict[laborForm.studentSupervisee_id] = []
-        laborFormDict[laborForm.studentSupervisee_id].append({"positionTitle": laborForm.POSN_TITLE, 
-                                                              "termCode":laborForm.termCode_id, 
-                                                              "laborStart":laborForm.startDate, 
-                                                              "laborEnd":laborForm.endDate, 
-                                                              "jobType":laborForm.jobType, 
-                                                              "wls":laborForm.WLS,
-                                                              "termName": laborForm.termCode.termName})
+                                .order_by(LaborStatusForm.termCode_id))
 
-    return jsonify(laborFormDict)
-
-def getFormsForStudent(bNumber):
-    # TODO: Write test 
-
-    givenOrgCode = (Department.select(Department.departmentID)
-                              .where(Department.ORG == 2084))
-    
-    deptLabor = (LaborStatusForm.select(LaborStatusForm.studentSupervisee_id, 
-                                        LaborStatusForm.termCode, 
-                                        LaborStatusForm.POSN_TITLE,
-                                        LaborStatusForm.startDate, 
-                                        LaborStatusForm.endDate,  
-                                        LaborStatusForm.jobType, 
-                                        LaborStatusForm.WLS,
-                                        )
-                                .join(Term).switch(LaborStatusForm)
-                                .join(FormHistory)
-                                .where(LaborStatusForm.studentSupervisee_id == bNumber,
-                                       LaborStatusForm.department_id.in_(givenOrgCode), 
-                                       FormHistory.status_id == "Approved")
-                                .order_by(LaborStatusForm.termCode_id))        
+    if bNumber: 
+        deptLabor = deptLabor.where(LaborStatusForm.studentSupervisee_id == bNumber)        
 
     laborFormDict = {}
     for laborForm in deptLabor:
