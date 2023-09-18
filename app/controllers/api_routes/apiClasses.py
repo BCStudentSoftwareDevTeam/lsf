4f698c0ba5abf7bc0e7518d235292227d86cc946
Andersonstettner <stettnera30@gmail.com>
2023-09-18 11:55:23 -0400

    Added some authentication for the api calls

--- app/controllers/api_routes/apiClasses.py
+++ app/controllers/api_routes/apiClasses.py
@@ -1,5 +1,8 @@
-from flask import abort
+from flask import abort, request
 from flask_restful import Resource
+
+from app import app
+from app.config.loadConfig import get_secret_cfg
 
 from app.models.department import Department
 from app.models.student import Student
@@ -7,6 +10,12 @@ from app.logic.apiEndpoint import getLaborInformation
 
 class LaborFormsForDepartmentApi(Resource):
     def get(self, orgCode): 
+        appConfig = get_secret_cfg()
+        requestersIp = request.remote_addr
+
+        if app.config['ENV'] == 'production' and not requestersIp == appConfig['VALID_REQUEST_IP']:
+            abort(403)
+
         if Department.select().where(Department.ORG == orgCode).exists():
             dptForms = getLaborInformation(orgCode = orgCode)
             return dptForms
@@ -15,6 +24,12 @@ class LaborFormsForDepartmentApi(Resource):
 
 class LaborFormsForStudentApi(Resource):
     def get(self, bNumber):
+        appConfig = get_secret_cfg()
+        requestersIp = request.remote_addr
+
+        if app.config['ENV'] == 'production' and not requestersIp == appConfig['VALID_REQUEST_IP']:
+            abort(403)
+
         if Student.select().where(Student.ID == bNumber).exists():
             stuForms = getLaborInformation(bNumber = bNumber)
             return stuForms
--- app/controllers/api_routes/routes.py
+++ app/controllers/api_routes/routes.py
@@ -1,6 +1,5 @@
 from app.controllers.api_routes.apiClasses import LaborFormsForDepartmentApi, LaborFormsForStudentApi
 
 def initializeApiRoutes(api):
-    # TODO: Add authentication
     api.add_resource(LaborFormsForDepartmentApi, '/api/org/<orgCode>')
     api.add_resource(LaborFormsForStudentApi, '/api/usr/<bNumber>')
