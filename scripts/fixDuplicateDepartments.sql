7b3af73f0c877f891d73290f5ca9cb21976fcaf6
hoerstl <hoerstl@berea.edu>
2023-11-15 11:04:12 -0500

    Added some more department fixes to the sql file

--- scripts/fixDuplicateDepartments.sql
+++ scripts/fixDuplicateDepartments.sql
@@ -15,6 +15,30 @@ UPDATE laborstatusform SET department_id=172 WHERE department_id=28 or departmen
 UPDATE department SET departmentCompliance=1 WHERE departmentID=172;
 DELETE FROM department WHERE departmentID=28 or departmentID=168;
 
+-- Make id 173 the official id for all `Craft Creative ACP Student Labor - St. P/R - Woodcraft`
+UPDATE laborstatusform SET department_id=173 WHERE department_id=15 or department_id=162;
+UPDATE department SET departmentCompliance=1 WHERE departmentID=173;
+DELETE FROM department WHERE departmentID=15 or departmentID=162;
+
+-- Make id 175 the official id for all `Student Success and Transition`
+UPDATE laborstatusform SET department_id=175 WHERE department_id=127 or department_id=163;
+UPDATE department SET departmentCompliance=1 WHERE departmentID=175;
+DELETE FROM department WHERE departmentID=127 or departmentID=163;
+
+
+-- Make id 177 the official id for all `VC&S and Log House Student Labor`
+UPDATE laborstatusform SET department_id=177 WHERE department_id=164;
+UPDATE department SET departmentCompliance=1 WHERE departmentID=177;
+DELETE FROM department WHERE departmentID=164;
+
+-- Make id 146 the official id for all `MAC Office`
+UPDATE laborstatusform SET department_id=146 WHERE department_id=102;
+UPDATE department SET departmentCompliance=1 WHERE departmentID=146;
+DELETE FROM department WHERE departmentID=102;
+
+
+
+
 
 /*
 -- Make id  the official id for all ``
