33b6155371b60a1cae9febed716c91e6c3e47bf2
hoerstl <hoerstl@berea.edu>
2023-11-28 17:59:05 -0500

    Added even more departments to the sql script.

--- scripts/fixDuplicateDepartments.sql
+++ scripts/fixDuplicateDepartments.sql
@@ -36,8 +36,35 @@ UPDATE laborstatusform SET department_id=146 WHERE department_id=102;
 UPDATE department SET departmentCompliance=1 WHERE departmentID=146;
 DELETE FROM department WHERE departmentID=102;
 
+-- Make id 181 the official id for all `CELTS - Off Campus Work`
+UPDATE laborstatusform SET department_id=181 WHERE department_id=109;
+UPDATE department SET departmentCompliance=1 WHERE departmentID=181;
+DELETE FROM department WHERE departmentID=109;
 
+-- Make id 174 the official id for all `Office Disability & Accessability`
+UPDATE laborstatusform SET department_id=174 WHERE department_id=49 or department_id=167;
+UPDATE department SET departmentCompliance=1 WHERE departmentID=174;
+DELETE FROM department WHERE departmentID=49 or departmentID=167;
 
+-- Make id 140 the official id for all `Risk Management`
+UPDATE laborstatusform SET department_id=140 WHERE department_id=42;
+UPDATE department SET departmentCompliance=1 WHERE departmentID=140;
+DELETE FROM department WHERE departmentID=42;
+
+-- Make id 144 the official id for all `Finance Office`
+UPDATE laborstatusform SET department_id=144 WHERE department_id=31;
+UPDATE department SET departmentCompliance=1 WHERE departmentID=144;
+DELETE FROM department WHERE departmentID=31;
+
+-- Make id 137 the official id for all `Student Life Administrative Office`
+UPDATE laborstatusform SET department_id=137 WHERE department_id=61;
+UPDATE department SET departmentCompliance=1 WHERE departmentID=137;
+DELETE FROM department WHERE departmentID=61;
+
+-- Make id 160 the official id for all `ACP General Administration`
+UPDATE laborstatusform SET department_id=160 WHERE department_id=27;
+UPDATE department SET departmentCompliance=1 WHERE departmentID=160;
+DELETE FROM department WHERE departmentID=27;
 
 
 /*
