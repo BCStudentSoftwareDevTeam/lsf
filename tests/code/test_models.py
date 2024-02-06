11b1fa767e7265cb49194dcaf378783dfbc0d47e
bineta doucoure <doucoureb@berea.edu>
2024-02-06 14:54:38 -0700

    all test passed

--- tests/code/test_models.py
+++ tests/code/test_models.py
@@ -58,28 +58,32 @@ def test_term_model():
     with mainDB.atomic() as transaction:
         # We expect that term codes will be ordered by year with ties broken by the last two digits in this order:
         # '13', '03', '12', '02', '01', '04', '11', [default], '00'
-        sortedSeasonCodes = ['13', '03', '05', '12', '02', '01', '04', '11', '99', '00']
+        correctlyOrderedSeasonCodes = ['13', '03', '05', '12', '02', '01', '04', '11', '99', '00']
 
         # Create the forms out of order
-        unsortedSeasonCodes = ['13', '02', '04', '00', '11', '12', '03', '99', '01', '05']
-        for seasonCode in unsortedSeasonCodes:
+        outOfOrderSeasonCodes = ['13', '02', '04', '00', '11', '12', '03', '99', '01', '05']
+        for seasonCode in outOfOrderSeasonCodes:
             createLSFandFormHistoryObj(termCode=int(f'2025{seasonCode}'))
 
         newForms = FormHistory.select(FormHistory, LaborStatusForm.termCode).join(LaborStatusForm, JOIN.LEFT_OUTER).where(FormHistory.rejectReason == "testing")
         sortedForms = Term.order_by_term(newForms.objects())
         resultingTermCodes = [str(f.termCode) for f in sortedForms]
         resultingSeasonalCodes = [termCode[4:] for termCode in resultingTermCodes]
-        assert resultingSeasonalCodes == sortedSeasonCodes
-
-
-        # Test that the year has more weight in the sort than the seasonal code
-        
+        assert resultingSeasonalCodes == correctlyOrderedSeasonCodes      
 
         
-
         transaction.rollback()
-    
-    
-    
+
+         # Test that the year has more weight in the sort than the seasonal code
+        sortedTermCodes = [202313, 202302, 202311, 202403, 202412, 202400]
+        unsortedTermCodes = [202302, 202403, 202313, 202311, 202400, 202412]
+        for termCodes in unsortedTermCodes:
+            createLSFandFormHistoryObj(termCode=int(termCodes))
+        newForms = FormHistory.select(FormHistory, LaborStatusForm.termCode).join(LaborStatusForm, JOIN.LEFT_OUTER).where(FormHistory.rejectReason == "testing")
+        sortedForms = Term.order_by_term(newForms.objects())
+        resultingTermCodes = [f.termCode for f in sortedForms]
+        assert  resultingTermCodes == sortedTermCodes
+        transaction.rollback()
+
     
 
