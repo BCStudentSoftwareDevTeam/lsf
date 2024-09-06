59ea5c9b1d1a7587c0207ff0c129068655776dac
bledsoef <bledsoef@berea.edu>
2024-09-06 04:36:28 +0000

    fixed sorting columns by converting properties to strings

--- app/controllers/main_routes/main_routes.py
+++ app/controllers/main_routes/main_routes.py
@@ -112,8 +112,8 @@ def getDatatableData(request):
     # It is used for identifying the column that needs to be sorted
     colIndexColNameMap = {  0: Term.termCode,
                             1: Department.DEPT_NAME,
-                            2: Supervisor.FIRST_NAME,
-                            3: Student.FIRST_NAME,
+                            2: Supervisor.FIRST_NAME.__str__(),
+                            3: Student.FIRST_NAME.__str__(),
                             4: LaborStatusForm.POSN_CODE,
                             5: LaborStatusForm.weeklyHours,
                             6: LaborStatusForm.startDate,
@@ -205,7 +205,8 @@ def getDatatableData(request):
     
     else:
         filteredSearchResults = formSearchResults.order_by(colIndexColNameMap[sortColIndex]).limit(rowsPerPage).offset(rowNumber)
-
+    print(colIndexColNameMap[sortColIndex])
+    print("__________________________________________ FASFDDASF FIlterd search results", filteredSearchResults)
     formattedData = getFormattedData(filteredSearchResults)
     formsDict = {"draw": draw, "recordsTotal": recordsTotal, "recordsFiltered": recordsTotal, "data": formattedData}
 
