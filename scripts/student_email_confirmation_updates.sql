eff82e9cd34340581b9ad0dee1f7471c6c8fb5f5
Brian Ramsay <ramsayb2@berea.edu>
2025-03-14 15:22:54 -0400

    Add supervisor email text

--- scripts/student_email_confirmation_updates.sql
+++ scripts/student_email_confirmation_updates.sql
@@ -1,13 +1,9 @@
 BEGIN;
 
--- XXX TEMPORARY set an open term
-INSERT INTO `term` (`termCode`, `termName`, `termState`, `isBreak`, `isSummer`, `isAcademicYear`, `isFinalEvaluationOpen`, `isMidyearEvaluationOpen`) VALUES (202500, 'AY 2025-2026', 0, 0, 0, 1, 0, 0);
-UPDATE `term` SET `termName` = 'AY 2025-2026', `termStart` = '2025-08-01', `termEnd` = '2026-05-01', `primaryCutOff` = '2025-09-04', `adjustmentCutOff` = '2025-09-12', `termState` = 1, `isBreak` = 0, `isSummer` = 0, `isAcademicYear` = 1, `isFinalEvaluationOpen` = 0, `isMidyearEvaluationOpen` = 0 WHERE (`term`.`termCode` = 202500);
-
 -- Update Denied statuses
 
--- XXX INSERT INTO status VALUES ('Denied by Admin');
--- XXX INSERT INTO status VALUES ('Denied by Student');
+INSERT INTO status VALUES ('Denied by Admin');
+INSERT INTO status VALUES ('Denied by Student');
 
 update overloadform set financialAidApproved_id='Denied by Admin' where financialAidApproved_id='Denied';
 update overloadform set SAASApproved_id='Denied by Admin' where SAASApproved_id='Denied';
@@ -49,5 +45,34 @@ SET
 WHERE purpose = 'Labor Status Form Submitted For Student';
 
 -- TODO Supervisor email template
+
+UPDATE emailtemplate
+SET
+    body = '
+<p>Dear @@Supervisor@@,</p>
+
+<p>ATTN:</p>
+
+<p>This email is confirmation that the Labor Program Office has received a Labor Status Form by <strong>@@Creator@@</strong> for <strong>@@Student@@</strong>. Please take a moment to read carefully and review the information below.</p>
+
+<p>The student has until <strong>@@StudentConfirmationExpiration@@</strong> to accept or deny this position. After that time the contract will be denied and a new labor status form will need to be submitted if you still wish to work with the student.</p>
+
+<p>NOTICE: This does not mean the position is active to begin work, only a status form has been submitted to await approval. Once this position has been approved, the student’s job will be active to allow for time entry in 24 hours. If at that time, the student cannot clock in, please contact the Labor Program Office immediately.</p>
+<p>If you have any further questions or concerns, contact the Labor Program Office at ext. 3611.</p>
+
+<h3>Labor Status Form Information:</h3>
+<p>Student Name and B-number: <strong>@@Student@@, @@StudB@@</strong></p>
+<p>Supervisor: <strong>@@Supervisor@@</strong></p>
+<p>Position Code/Title: <strong>@@Position@@</strong></p>
+<p>WLS Level: <strong>@@WLS@@</strong></p>
+<p>Department Name: <strong>@@Department@@</strong></p>
+<p>Hours per Week (Total Contracted Hours for Break Periods): <strong>@@Hours@@</strong></p>
+<p>Begin Date: <strong>@@Date@@</strong></p>
+<br>
+<p>Sincerely,</p>
+<p>Labor Program Office</p>
+<p>labor_program@berea.edu</p>
+<p>859-985-3611</p>'
+WHERE purpose = 'Primary Position Labor Status Form Submitted';
 
 COMMIT;
