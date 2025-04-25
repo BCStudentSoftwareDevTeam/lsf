da134a348b1625d6e18aca8799059487c33169fd
Karina <karinaar4299@gmail.com>
2025-04-25 11:06:33 -0400

    Added path for oracle instant client

--- setup.sh
+++ setup.sh
@@ -39,3 +39,11 @@ export FLASK_APP=app.py         # app entry point
 export FLASK_ENV=development    # default
 export FLASK_RUN_PORT=8080      # For consistency (python app.py vs flask run)
 export FLASK_RUN_HOST=0.0.0.0   # To allow external routing to the application
+
+ORACLE_BASE=/home/oracle/instantclient_19_9/
+ORACLE_HOME=/home/oracle/instantclient_19_9/
+LD_LIBRARY_PATH=$ORACLE_HOME
+#JAVA_HOME=<path_to_java_home>
+PATH=$PATH:$ORACLE_HOME
+export ORACLE_BASE ORACLE_HOME PATH LD_LIBRARY_PATH
+
