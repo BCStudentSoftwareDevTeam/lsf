f3c64bf36fdd644777d3ae7becbeb49a5910472c
Brian Ramsay <ramsayb2@berea.edu>
2025-08-25 11:22:01 -0400

    Make sure the base data email template matches the one in production

--- database/base_data.py
+++ database/base_data.py
@@ -45,25 +45,24 @@ emailtemps= [
                 "action":"Submitted",
                 "subject":"Labor Status Form Received",
                 "body":'''<p>Dear <strong>@@Student@@</strong>,</p>
-                            <p>&nbsp;</p>
-                            <p>This email is very important. Please take a moment to read carefully and review the information. A Labor Status Form has been submitted for you by <strong>@@Creator@@</strong>. Below is the position information for which you have been hired. If you do not accept the terms of this form, you will have 24 hours to contact the supervisor or the Labor Program Office. If we do not hear from you within 24 hours of this notification, it will be determined that it is accepted and the forms will be processed as submitted.</p>
-                            <p>&nbsp;</p>
-                            <p><strong>NOTICE:</strong> This does not mean your position is active to begin work, only a status form has been submitted to await approval. Once this position has been approved, your job will be active to allow for time entry in 24 hours. If at that time, you cannot clock in, please contact the Labor Program Office immediately.</p>
-                            <p>&nbsp;</p>
-                            <p>If you have any further questions or concerns, contact the Labor Program Office at ext. 3611.</p>
-                            <p>&nbsp;</p>
-                            <p>&nbsp;</p>
-                            <p><strong>Labor Status Form Information:</strong></p>
-                            <p>Position Code/Title: <strong>@@Position@@</strong></p>
-                            <p>Department Name: <strong>@@Department@@</strong></p>
-                            <p>Hours per Week (Total Contracted Hours for Break Periods): <strong>@@Hours@@</strong></p>
-                            <p>Begin Date: <strong>@@Date@@</strong></p>
-                            <p>&nbsp;</p>
-                            <p>&nbsp;</p>
-                            <p>Sincerely,</p>
-                            <p>Labor Program Office</p>
-                            <p>labor_program@berea.edu</p>
-                            <p>859-985-3611</p>''',
+                          <p><strong>ACTION IS REQUIRED - This offer will EXPIRE on @@StudentConfirmationExpiration@@.</strong></p> 
+                          <p>This email is very important. Please take a moment to read carefully and review the information.  
+                          A Labor Status Form has been submitted by @@Creator@@ for you,for a position supervised by <strong>@@Supervisor@@.</strong></p> 
+                          <p><strong>Labor Status Form Information:</strong></p>
+                          <p>Student Name: <strong>@@Student@@ @@StudB@@</strong></p>
+                          <p>Supervisor: <strong>@@Supervisor@@</strong></p>
+                          <p>Position Code/Title: <strong>@@Position@@ (WLS @@WLS@@)</strong></p>
+                          <p>Department Name: <strong>@@Department@@</strong></p>
+                          <p>Hours per Week (Total Contracted Hours for Break Periods): <strong>@@Hours@@</strong></p>
+                          <p>Begin Date: <strong>@@Date@@</strong></p>
+                          <p><strong>Please review the labor contract and respond using the following link:</strong></p>
+                          <p><a href=\"@@StudentConfirmationLink@@\">Click here to approve or deny the labor contract</a></p>
+                          <p>Your approval or denial is required to process this contract. After @@StudentConfirmationExpiration@@ the contract will be considered denied and your supervisor will need to submit a new contract, if the position is still available.</p>
+                          <p>If you have any further questions or concerns, contact our Labor Program Office at labor_program@berea.edu or call us at ext. 3611.</p>
+                          <p>Sincerely,</p>
+                          <p>Labor Program Office</p>
+                          <p>labor_program@berea.edu</p>
+                          <p>859-985-3611</p>''',
 
                 "audience":"Student"
                  },
