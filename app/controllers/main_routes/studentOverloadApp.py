956be68896a55262b3e477a769c299af6d90120b
zawn <zawn@berea.edu>
2025-09-23 21:28:25 +0000

    "just committing"

--- app/controllers/main_routes/studentOverloadApp.py
+++ app/controllers/main_routes/studentOverloadApp.py
@@ -1,6 +1,6 @@
-from datetime import date
+from datetime import date, datetime
 from playhouse.shortcuts import model_to_dict
-from flask import json, jsonify, request, redirect, url_for, abort, flash
+from flask import json, jsonify, request, redirect, url_for, abort, flash, g
 
 from app.controllers.main_routes import *
 from app.logic.emailHandler import*
@@ -62,7 +62,7 @@ def studentOverloadApp(formHistoryId):
         studentPrimaryHistory = (FormHistory.select().where(
                                     FormHistory.formID == primaryForm,
                                     FormHistory.historyType == "Labor Status Form",
-                                    FormHistory.status.in_(["Approved","Pending"]) ))
+                                    FormHistory.status.in_(["Approved","Pending","Pre-Student Approval"]) ))
         formIDPrimary.append(studentPrimaryHistory)
     formIDSecondary = []
 
@@ -71,16 +71,17 @@ def studentOverloadApp(formHistoryId):
                                     FormHistory.formID == secondaryForm,
                                     FormHistory.historyType == "Labor Status Form",
                                     FormHistory.status.in_(["Approved","Pending"]) ))
+                                    FormHistory.status.in_(["Approved","Pending","Pre-Student Approval"]) ))
         formIDSecondary.append(studentSecondaryHistory)
 
     totalCurrentHours = 0
     for i in formIDPrimary:
         for j in i:
-            if str(j.status) != "Pending":
+            if str(j.status) == "Approved":
                 totalCurrentHours += j.formID.weeklyHours
     for i in formIDSecondary:
         for j in i:
-            if str(j.status) != "Pending":
+            if str(j.status) == "Approved":
                 totalCurrentHours += j.formID.weeklyHours
     totalFormHours = totalCurrentHours + prefillHoursOverload
 
@@ -139,10 +140,18 @@ def updateDatabase(overloadFormHistoryID):
                                            .where(FormHistory.historyType_id == "Labor Status Form")).get()
 
         with mainDB.atomic() as transaction:
+            # Update statuses
             overloadFormHistory.status = newStatus
             overloadFormHistory.save()
             originalFormHistory.status = newStatus
             originalFormHistory.save()
+
+            # Update base student confirmation
+            originalFormHistory.formID.studentResponseDate = datetime.now()
+            originalFormHistory.formID.studentConfirmation = True
+            originalFormHistory.formID.save()
+
+            # Update overload form
             overloadForm = overloadFormHistory.overloadForm
             overloadForm.studentOverloadReason = overloadReason
             overloadForm.save()
@@ -150,8 +159,9 @@ def updateDatabase(overloadFormHistoryID):
             email = emailHandler(overloadFormHistory.formHistoryID)
             link = makeThirdPartyLink("Financial Aid", request.host, overloadFormHistory.formHistoryID)
             email.overloadVerification("Financial Aid", link)
-        currentUser = require_login()
-        return (currentUser.student.ID)
+
+        flash("Overload Request Submitted", "success")
+        return g.currentUser.student.ID
 
     except Exception as e:
         print("ERROR: " + str(e))
