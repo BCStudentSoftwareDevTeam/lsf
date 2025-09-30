67ee7580009945fbe5e05c462536917bf05e2082
Johncox2211 <johncox428@yahoo.com>
2025-09-30 19:42:59 +0000

    removed duplicate getUser function, also removed unused add/remove admin function imports

--- app/controllers/admin_routes/adminManagement.py
+++ app/controllers/admin_routes/adminManagement.py
@@ -8,7 +8,7 @@ from app.models.supervisor import Supervisor
 from app.models.student import Student
 from app.logic.tracy import Tracy
 from app.logic.userInsertFunctions import createStudentFromTracy, createSupervisorFromTracy, createUser
-from app.logic.adminManagement import searchForAdmin,  getUser, addAdmin, removeAdmin
+from app.logic.adminManagement import searchForAdmin, getUser
 from app.logic.utils import adminFlashMessage
 
 
@@ -75,20 +75,6 @@ def addAdmin(user, adminType):
 def removeAdmin(user, adminType):
     setattr(user, f"is{adminType.capitalize()}Admin", False)
     user.save()
-
-def getUser(selectpickerID):
-    username = request.form.get(selectpickerID)
-    try:
-        user = User.get(User.username == username)
-    except DoesNotExist as e:
-        usertype = Tracy().checkStudentOrSupervisor(username)
-        supervisor = student = None
-        if usertype == "Student":
-            student = createStudentFromTracy(username)
-        else:
-            supervisor = createSupervisorFromTracy(username)
-        user = createUser(username, student=student, supervisor=supervisor)
-    return user
 
 def flashMessage(user, action, adminType):
     message = "{} has been {} as a {} Admin".format(user.fullName, action, adminType)
