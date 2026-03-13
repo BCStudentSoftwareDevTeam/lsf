e0345b8f6aa6f3f1288025cc5ef6b5628d31a458
zawn <zawn@berea.edu>
2026-03-13 17:12:37 -0400

    sidebar responsivness being worked on

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -7,4 +7,30 @@ $(document).ready(function() {
     $('.navbar li').removeAttr('tabindex');
     $('.navbar div').removeAttr('tabindex');
     }
+
+  function openSidebar() {
+    $('#sidebar').addClass('sidebar-open');
+    $('#sidebar-overlay').addClass('active');
+    $('body').addClass('sidebar-is-open');
+    $('#sidebar-toggle').attr('aria-expanded', true);
+  }
+
+  function closeSidebar() {
+    $('#sidebar').removeClass('sidebar-open');
+    $('#sidebar-overlay').removeClass('active');
+    $('body').removeClass('sidebar-is-open');
+    $('#sidebar-toggle').attr('aria-expanded', false);
+  }
+
+  $('#sidebar-toggle').on('click', function() {
+    if ($('#sidebar').hasClass('sidebar-open')) {
+      closeSidebar();
+    } else {
+      openSidebar();
+    }
   });
+
+  $('#sidebar-overlay').on('click', function() {
+    closeSidebar();
+  });
+});
