ed91c502337f86274764ea86bbaaf0ed5a748d64
glek <156453486+Kafui123@users.noreply.github.com>
2026-04-01 04:10:25 +0000

    fixed some tests

--- tests/code/test_adminManagement.py
+++ tests/code/test_adminManagement.py
@@ -5,42 +5,54 @@ from peewee import DoesNotExist
 
 @pytest.mark.integration
 def test_addAdmin():
-    newAdmin = "pearcej"
+    newAdmin = "jonesj"
     user = User.get(User.username == newAdmin)
-
     # Before adding user as admin
     assert not user.isLaborAdmin 
     # Test adding labor admin
-    addAdmin(user, 'labor')
+    addAdmin(user, 'Labor')
+    user = User.get(User.username == newAdmin)
     assert user.isLaborAdmin
 
     assert not user.isFinancialAidAdmin
+    
     # Test adding financial aid admin
-    addAdmin(user, 'finAid')
+    addAdmin(user, 'FinancialAid')
+    
     assert user.isFinancialAidAdmin
 
     assert not user.isSaasAdmin
     # Test adding saas admin
-    addAdmin(user, 'saas')
+    addAdmin(user, 'Saas')
     assert user.isSaasAdmin
+    
+    # cleanup
+    user.isLaborAdmin = False
+    user.isFinancialAidAdmin = False
+    user.isSaasAdmin = False
+    user.save()
 
 @pytest.mark.integration
 def test_removeAdmin():
     oldAdmin = "pearcej"
     user = User.get(User.username == oldAdmin)
-
+    addAdmin(user, 'Labor')
     # Before removing user as admin
     assert user.isLaborAdmin
     # Test removing labor admin
-    removeAdmin(user, 'labor')
+    removeAdmin(user, 'Labor')
     assert not user.isLaborAdmin
 
+    addAdmin(user, 'FinancialAid')
     assert user.isFinancialAidAdmin
     # Test removing financial aid admin
-    removeAdmin(user, 'finAid')
+    removeAdmin(user, 'FinancialAid')
     assert not user.isFinancialAidAdmin
 
+    addAdmin(user, 'Saas')
     assert user.isSaasAdmin
     # Test removing saas admin
-    removeAdmin(user, 'saas')
+    removeAdmin(user, 'Saas')
     assert not user.isSaasAdmin
+    
+
