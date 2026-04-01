463b53bad6d168c6f4b3c90171ef130e60921f08
Johncox2211 <johncox428@yahoo.com>
2026-04-01 21:01:31 +0000

    implemented Rue's suggested change

--- app/logic/allPendingForms.py
+++ app/logic/allPendingForms.py
@@ -283,10 +283,15 @@ def checkAdjustment(allForms):
             allForms.adjustedForm.oldValue = newPosition.POSN_TITLE
 
         if allForms.adjustedForm.fieldAdjusted == "department":
-            newDepartment = Department.get_or_none(Department.ORG==allForms.adjustedForm.newValue)
+            newDepartment = Department.get_or_none(Department.ORG == allForms.adjustedForm.newValue)
+            
             if newDepartment:
                 allForms.adjustedForm.newValue = newDepartment.DEPT_NAME
-                allForms.adjustedForm.oldValue = newDepartment.ORG + "-" + newDepartment.ACCOUNT
             else:
-                allForms.adjustedForm.newValue = "Unknown" + " - " + allForms.adjustedForm.newValue
-                allForms.adjustedForm.oldValue = "Unknown" + " - " + allForms.adjustedForm.oldValue
+                allForms.adjustedForm.newValue = "Unknown - " + allForms.adjustedForm.newValue
+
+            oldDepartment = Department.get_or_none(Department.ORG == allForms.adjustedForm.oldValue)
+            if oldDepartment:
+                allForms.adjustedForm.oldValue = oldDepartment.ORG + "-" + oldDepartment.ACCOUNT
+            else:
+                allForms.adjustedForm.oldValue = "Unknown - " + allForms.adjustedForm.oldValue
