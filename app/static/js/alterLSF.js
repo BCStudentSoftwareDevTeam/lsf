d59ab5424cf0864e7006a611008192c3d51e1ff6
conwelld <conwelld@berea.edu>
2026-06-16 19:21:25 +0000

    replaced labor program with Berea's Work college program in app

--- app/static/js/alterLSF.js
+++ app/static/js/alterLSF.js
@@ -73,7 +73,7 @@ function fillHoursPerWeek(){ // prefill hours per week select picker
         $("#warningModalText").html("Student with WLS Level 5 or 6 must have at least a 15 hour contract. " +
                                  "These positions require special authorization as specified at " +
                                  "<a href=\"http://catalog.berea.edu/2014-2015/Tools/Work-Learning-Service-Levels-WLS\""+
-                                 "target=\"_blank\">The Labor Program Website.</a>");
+                                 "target=\"_blank\">Berea's Work College Program Website.</a>");
         $("#warningModalButton").css('display', 'none');
         $("#resetConfirmButton").css('display', 'none');
         $("#warningModal").modal("show");
