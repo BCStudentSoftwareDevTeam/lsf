2a0ff359cd1eeb0457d3137ed8d1020c39fd8fe1
carnesa <carnesa@berea.edu>
2026-06-18 16:00:25 +0000

    Updated sidebar JQueiry script to change the html text on the Administration tab to change carets if the admin toggle is open or closed.

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -17,6 +17,14 @@ $(document).ready(function() {
   $('#sidebar-overlay').on('click', function() {
     closeSidebar();
   });
+  // Change the text of the admin-toggle pannel when the user clicks on it to the change unicode character.
+  $('#admin-toggle').on('click', function() {
+    if ($('#admin').hasClass('in')) { //Check if its class in or colapse in
+      toggleAdminPanelClose();
+    } else {
+      toggleAdminPanelOpen();
+    }
+  });
   });
 
 function openSidebar() {
@@ -31,6 +39,11 @@ function closeSidebar() {
   $('body').removeClass('sidebar-is-open');
   $('#sidebar-toggle').attr('aria-expanded', false);
 }
-
-
-
+// Change the text of the admin-toggle pannel when the user clicks on it to the open unicode character.
+function toggleAdminPanelOpen() {
+  $('#admin-toggle h4').html('Administration 🢑');
+}
+// Change the text of the admin-toggle pannel when the user clicks on it to the close unicode character.
+function toggleAdminPanelClose() {
+  $('#admin-toggle h4').html('Administration 🢓');
+}
