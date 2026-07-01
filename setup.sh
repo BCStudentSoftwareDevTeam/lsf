ff54bf7d0bc732c896f5214206ed7cdb8d886aa0
zawn <zawn@berea.edu>
2026-07-01 10:38:24 -0400

    as this works from the root as source setup must run from the root we just need tohave cd ..

--- setup.sh
+++ setup.sh
@@ -43,9 +43,9 @@ export FLASK_RUN_HOST=0.0.0.0   # To allow external routing to the application
 # Fix symbolic link inside database directory
 if ! [ -L "database/app" ]; then
 	echo "Fixing symlink to app in database directory"
-	pushd database > /dev/null
+	cd database
 	rm app
 	ln -s ../app
-	popd > /dev/null
+	cd ..
 fi
 
