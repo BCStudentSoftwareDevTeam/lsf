d44c4e517aaf41ce8495d91a15e44c807cc028df
zawn <zawn@berea.edu>
2026-07-01 10:35:53 -0400

    working on boxsdk

--- scripts/laborPositionMigration.py
+++ scripts/laborPositionMigration.py
@@ -1,9 +1,52 @@
-from markitdown import MarkItDown
+from io import BytesIO
+import mammoth
+from boxsdk import OAuth2, Client
 
-def parseDocument(document):
-    print("here")
-    md = MarkItDown()
-    print("lol")
-    print(md)
+auth = OAuth2(
+    client_id="YOUR_CLIENT_ID",
+    client_secret="YOUR_CLIENT_SECRET",
+    access_token="YOUR_ACCESS_TOKEN",
+)
 
-parseDocument(None)
+client = Client(auth)
+
+folder_id = "YOUR_FOLDER_ID"
+
+def getBoxFolder(folderID):
+    return client.folder(folderID).get()
+
+
+def getBoxFiles(folder):
+    return folder.get_items()
+
+
+def parseDocument(box_file):
+    memory_file = BytesIO()
+
+    box_file.download_to(memory_file)
+    memory_file.seek(0)
+    result = mammoth.extract_raw_text(memory_file)
+    return result.value
+
+def checkDocumentExist(documentName):
+    # TODO: Check if the document already exists in your database
+    return False
+
+def migrateDocument():
+    folder = getBoxFolder(folder_id)
+    for item in getBoxFiles(folder):
+        if item.type != "file":
+            continue
+        if not item.name.endswith(".docx"):
+            continue
+        print(f"Processing: {item.name}")
+        if checkDocumentExist(item.name):
+            print("Already exists.")
+            continue
+        text = parseDocument(item)
+        print(text)
+
+        # TODO:
+        # Save text to database here
+
+migrateDocument()
