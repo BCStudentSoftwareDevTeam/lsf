65c6b0338b29ec8f8267d7a2068672121b6957c0
feitsopb <feitsopb@berea.edu>
2026-07-02 20:27:51 +0000

    added an HTML file for manage people added the main routes to the page

--- app/controllers/main_routes/departmentPortal.py
+++ app/controllers/main_routes/departmentPortal.py
@@ -1,1 +1,39 @@
-from flask import render_template
+from flask import render_template, request, json, redirect, url_for, send_file, g, flash, jsonify
+from peewee import JOIN, DoesNotExist
+from functools import reduce
+import operator
+from app.models.department import Department
+from app.models.supervisor import Supervisor
+from app.models.supervisorDepartment import SupervisorDepartment
+from app.models.student import Student
+from app.models.laborStatusForm import LaborStatusForm
+from app.models.formHistory import FormHistory
+from app.models.term import Term
+from app.controllers.admin_routes.allPendingForms import checkAdjustment
+from app.controllers.main_routes import main_bp
+from app.logic.download import CSVMaker, saveFormSearchResult, retrieveFormSearchResult
+from app.logic.search import getDepartmentsForSupervisor, searchPerson, searchSupervisorPortal
+from app.login_manager import require_login, logout
+from app.logic.getTableData import getDatatableData
+from app.logic.banner import Banner
+
+@main_bp.route('/department/manage_staff', methods=['GET'])
+@main_bp.route('/department/manage_staff/<org>', methods=['GET'])
+@main_bp.route('/department/manage_staff/<org>/<account>', methods=['GET'])
+def departmentPortal(org=None,account=None):
+    try:
+        dept = Department.get(Department.ORG == org, Department.ACCOUNT == account)
+    except (NameError, DoesNotExist):
+        dept = None
+
+
+
+    if g.currentUser.isLaborAdmin:
+        departments = list(Department.select().order_by(Department.isActive.desc(), Department.DEPT_NAME.asc()))
+    else:
+        departments = list(getDepartmentsForSupervisor(g.currentUser).order_by(Department.isActive.desc(), Department.DEPT_NAME.asc()))
+
+    return render_template('main/departmentPortal.html', 
+                           departments = departments,
+                           department = dept)
+
