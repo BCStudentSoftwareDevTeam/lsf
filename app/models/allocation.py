b760623f55458eb2833488846839c8d6362d7e7b
rukwashai <rukwashai@berea.edu>
2026-07-06 10:37:14 -0400

    Revert "Add real data to the Current Allocation card"
    This reverts commit abcd951f493146b284bae8c55717d8c598dc0862.

--- app/models/allocation.py
+++
@@ -1,9 +0,0 @@
-from app.models import *
-from app.models.department import Department
-from app.models.term import Term
-
-class Allocation(baseModel):
-    department      = ForeignKeyField(Department, on_delete="cascade")
-    term            = ForeignKeyField(Term, on_delete="cascade")
-    totalPositions  = IntegerField()
-    totalBreakHours = IntegerField()
