082569ec80a9ba49f7af92596056a5bf5e959b56
fritzj2 <fritzj2@berea.edu>
2026-07-08 15:22:11 +0000

    added the position history to the demo data to test the links

--- database/demo_data.py
+++ database/demo_data.py
@@ -17,6 +17,7 @@ from app.models.term import Term
 from app.models.laborStatusForm import LaborStatusForm
 from app.models.formHistory import FormHistory
 from app.models.notes import Notes
+from app.models.positionHistory import PositionHistory
 
 print("Inserting data for demo and testing purposes")
 
@@ -609,3 +610,63 @@ notes = [
        ]
 Notes.insert_many(notes).on_conflict_replace().execute()
 print(" * laborOfficeNotes added")
+
+#############################
+# Position History
+#############################
+
+positionhistory = [
+    {
+        "positioncode": "S61407",
+        "status": "Active",
+        "WLS": 1,
+        "revisiondate": f"{current_year}-07-01",
+        "Description": "",
+        "Department_id": 1
+    },
+    {
+        
+        "positioncode": "S61408",
+        "status": "Active",
+        "WLS": 2,
+        "revisiondate": f"{current_year}-09-01",
+        "Description": "",
+        "Department_id": 1
+    },
+    {
+        "positioncode": "S61409",
+        "status": "Active",
+        "WLS": 3,
+        "revisiondate": f"{current_year}-07-01",
+        "Description": "",
+        "Department_id": 1
+    },
+    {
+        "positioncode": "S61411",
+        "status": "Active",
+        "WLS":3,
+        "revisiondate" : f"{current_year}-01-01",
+        "Description": "",
+        "Department_id" : 1
+
+        },
+        {
+        "positioncode": "S61410",
+        "status": "Inactive",
+        "WLS":2,
+        "revisiondate" : f"{current_year}-01-01",
+        "Description": "",
+        "Department_id" : 1
+        },
+
+        {
+        "positioncode": "S12345",
+        "status": "Active",
+        "WLS":3,
+        "revisiondate" : f"{current_year}-01-23",
+        "Description": "",
+        "Department_id" : 1
+        }
+    
+]
+PositionHistory.insert_many(positionhistory).on_conflict_replace().execute()
