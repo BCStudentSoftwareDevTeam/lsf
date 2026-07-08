afeef6c4f3e10a91ddbabf87bdac2ba1289a0e09
feitsopb <feitsopb@berea.edu>
2026-07-08 19:25:41 +0000

    added a flash message on status

--- app/static/js/manageMembers.js
+++ app/static/js/manageMembers.js
@@ -14,5 +14,31 @@ $(document).ready(function() {
         },
         //dom: '<"top"l>rt<"bottom"p><"clear">' 
     });
+
+    $(document).on("click", ".member-status-btn", function() {
+        let  memberName = $(this).data("member-name");
+        let memberStatus = $(this).data("member-status");
+        let category;
+
+if (memberStatus === "Banned") {
+    category = "danger";
+} else {
+    category = "success";
+}
+        let quote = String.fromCharCode(39);
+
+        $("#flash_container").html("<div class=\"alert alert-" + category + "\" role=\"alert\" id=\"flasher\">The status for " + memberName + " has been set to " + quote + memberStatus + quote + ".</div>");
+        $("#flasher").delay(3000).fadeOut();
+
+        if (memberStatus === "Banned") {
+            $(this).removeClass("btn-danger").addClass("btn-success");
+            $(this).text("Unban");
+            $(this).data("member-status", "Unbanned");
+        } else {
+            $(this).removeClass("btn-success").addClass("btn-danger");
+            $(this).html("&nbsp; Ban &nbsp;");
+            $(this).data("member-status", "Banned");
+        }
+    });
 })
 
