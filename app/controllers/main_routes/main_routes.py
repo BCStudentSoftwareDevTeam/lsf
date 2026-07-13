f09ee8ed9d5ef007d6a9c23b805225107f31d31d
John Lolonga <lolongaj@berea.edu>
2026-07-13 15:56:36 -0400

    fixed the if statement in the html and posURL

--- app/controllers/main_routes/main_routes.py
+++ app/controllers/main_routes/main_routes.py
@@ -67,20 +67,20 @@ def departmentPortal(org=None,account=None):
         
     positions = list(PositionHistory.select().where(PositionHistory.department == dept, PositionHistory.status == "Active").order_by(PositionHistory.positionTitle.asc())) if dept else []
     positionsList = []
-    posUrl = []
-    if not positions:
-        positionsList = ["No active positions in this department"]
+    posURL = []
+    if positions == []:
+        pass
     else:
         for i in positions:
             positionsList.append(i.positionTitle + ": " + "(WLS " + str(i.wls) + ")")
-            posUrl.append(str(i.positionCode))
+            posURL.append(str(i.positionCode))
 
 
     return render_template('main/departmentPortal.html', 
                            departments = departments,
                            department = dept,
                            positions = positionsList,
-                           posUrl = posUrl)
+                           posURL = posURL)
 
 @main_bp.route('/supervisorPortal/addUserToDept', methods=['GET', 'POST'])
 def addUserToDept():
