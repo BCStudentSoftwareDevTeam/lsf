7180a275e70d2b7b57443637380eb4496bb8c08d
BhushanSah <itsbhushansah@gmail.com>
2026-07-13 15:16:45 +0000

    Removed currentuser.login

--- app/controllers/main_routes/main_routes.py
+++ app/controllers/main_routes/main_routes.py
@@ -56,8 +56,6 @@ def departmentPortal(org=None,account=None):
     except (NameError, DoesNotExist):
         dept = None
 
-    currentUser = require_login()
-
     if g.currentUser.isLaborAdmin:
         departments = list(Department.select().order_by(Department.isActive.desc(), Department.DEPT_NAME.asc()))
     else:
@@ -106,7 +104,7 @@ def departmentPortal(org=None,account=None):
                            department = dept,
                            supervisors = supervisors,
                            labor_coordinators=labor_coordinators,
-                           currentUser=currentUser
+                           currentUser=g.currentUser
                            )
 
 @main_bp.route('/department/<org>/<account>/managepositions', methods=['GET'])
