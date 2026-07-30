05216d031c9281ede0d70206ff88284f516c1f75
fritzj2 <fritzj2@berea.edu>
2026-07-30 16:29:57 -0400

    added a download button to the page

--- app/static/js/allocationTable.js
+++ app/static/js/allocationTable.js
@@ -1,7 +1,15 @@
 $(document).ready( function(){
+    const entriesPerYear = 5;
+
     allocationTable = $('#allocationTable');
     allocationTable.DataTable({
+        paging: true,
         searching: true,
-        pageLength: 25
+        pageLength: 5,
+        lengthMenu:[[5,10,20,50,-1],
+        ['5', '10', '20', '50', 'All']],
+        "order": [
+            0, "desc"
+        ]
     });
 });
