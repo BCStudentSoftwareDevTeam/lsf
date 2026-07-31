beaba49b382ae93db30e4d00554527a9e1f72cfc
fritzj2 <fritzj2@berea.edu>
2026-07-31 16:11:08 -0400

    structured primaries table

--- app/static/js/allocationTable.js
+++ app/static/js/allocationTable.js
@@ -1,1 +1,10 @@
-$("#admin").collapse("show");
+$(document).ready( function(){
+    fallTermTable = $('#fallTermTable');
+    fallTermTable.DataTable({
+        pageLength: 25,
+        info: false,
+        lengthChange: false,
+        searching: false,
+        paging: false
+    });
+});
