e9811183e61bb5cd96b9b156c53254ed94490773
fritzj2 <fritzj2@berea.edu>
2026-07-31 16:56:28 -0400

    Formatted the break table

--- app/static/js/allocationTable.js
+++ app/static/js/allocationTable.js
@@ -7,4 +7,21 @@ $(document).ready( function(){
         searching: false,
         paging: false
     });
+
+    springTermTable = $('#springTermTable');
+    springTermTable.DataTable({
+        pageLength: 25,
+        info: false,
+        lengthChange: false,
+        searching: false,
+        paging: false
+    });
+    breakTable = $('#breakTable');
+    breakTable.DataTable({
+        pageLength: 25,
+        info: false,
+        lengthChange: false,
+        searching: false,
+        paging: false
+    });
 });
