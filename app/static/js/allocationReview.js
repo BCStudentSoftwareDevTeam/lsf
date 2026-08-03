fc218630c4d913fe88af8c43a33344c6c8b1e250
kurasova <kurasova@berea.edu>
2026-08-03 13:43:09 -0400

    Add popovers for the Allocation Review page

--- app/static/js/allocationReview.js
+++ app/static/js/allocationReview.js
@@ -1,6 +1,12 @@
 $(document).ready( function(){
+
+    $('[data-toggle="popover"]').popover();
+    
     // not allowing users to type anything in a numeric spinner
-    $("input[type='number'].numericSpinner").keypress(function (evt) {
+    $("input[type='number'].breakHoursNumericSpinner").keypress(function (evt) {
+      evt.preventDefault();
+    });
+    $("input[type='number'].positionNumericSpinner").keypress(function (evt) {
       evt.preventDefault();
     });
 });
