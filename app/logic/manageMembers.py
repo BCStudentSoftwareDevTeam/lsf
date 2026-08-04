51ee08ac97b3dc9368e30cf68088c4484eed421f
BhushanSah <itsbhushansah@gmail.com>
2026-08-04 13:15:19 -0400

    Made pewee command into one line

--- app/logic/manageMembers.py
+++ app/logic/manageMembers.py
@@ -26,11 +26,8 @@ def getActivePendingPositionCounts(dept, currentYear):
     )
 
     activePrimaries = ((LaborStatusForm.jobType == "Primary") & (FormHistory.status == "Approved"))
-
     pendingPrimaries = ((LaborStatusForm.jobType == "Primary") & (FormHistory.status.in_(pendingStatuses)))
-
     activeSecondaries = ((LaborStatusForm.jobType == "Secondary") & (FormHistory.status == "Approved"))
-
     pendingSecondaries = ((LaborStatusForm.jobType == "Secondary") & (FormHistory.status.in_(pendingStatuses)))
 
     rows = list(
