cdfbae51ba713ea6466bd1f34253543697800809
fritzj2 <fritzj2@berea.edu>
2026-08-05 11:51:52 -0400

    made getStudents() less brittle

--- tests/code/test_tracy.py
+++ tests/code/test_tracy.py
@@ -18,8 +18,11 @@ class Test_Tracy:
     def test_getStudents(self, tracy):
         with app.app_context():
             students = tracy.getStudents()
-            assert ['Elaheh','Guillermo','Jeremiah','Kat', 'Oluwagbayi', 'Test', 'Tyler'] == [s.FIRST_NAME for s in students]
-            assert ['718','300','420','420', '883', '700', '420'] == [s.STU_CPO for s in students]
+            for student in ['Elaheh','Guillermo','Jeremiah','Kat', 'Oluwagbayi', 'Test', 'Tyler']:
+                assert student in [s.FIRST_NAME for s in students]
+            for cpo in ['718','300','420','420', '883', '700', '420']:
+                assert cpo in [s.STU_CPO for s in students]
+
 
     @pytest.mark.integration
     def test_getStudentFromBNumber(self, tracy):
