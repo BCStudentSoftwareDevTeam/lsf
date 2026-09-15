11cd4faf670a76b1669a8c33a7172d285af8b22b
Rue Haile <hailer@berea.edu>
2026-09-15 16:24:22 -0400

    action button change

--- app/models/positionHistory.py
+++ app/models/positionHistory.py
@@ -2,6 +2,7 @@ from app.models import *
 from app.models.department import Department
 
 class PositionHistory(baseModel):
+    positionTitle      = CharField()
     positionCode       = CharField()
     department         = ForeignKeyField(Department)
     status             = CharField()
@@ -11,3 +12,4 @@ class PositionHistory(baseModel):
 
     class Meta:
         indexes = ( (('positionCode', 'revisionDate', 'status'), True), )
+
