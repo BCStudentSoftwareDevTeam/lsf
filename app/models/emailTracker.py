28c185d06d5a62acab626fdaf1221682117a670e
fritzj2 <fritzj2@berea.edu>
2026-09-15 08:24:45 -0400

    subject can now be null

--- app/models/emailTracker.py
+++ app/models/emailTracker.py
@@ -7,7 +7,7 @@ class EmailTracker(baseModel):
     formID             = ForeignKeyField(LaborStatusForm)               # foreign key to lsf
     date               = DateField()
     recipient          = CharField()
-    subject            = CharField()
+    subject            = CharField(null=True)
     template           = ForeignKeyField(EmailTemplate)                 # foreign key to email template
     recipientEmails    = TextField()
     body               = TextField()
